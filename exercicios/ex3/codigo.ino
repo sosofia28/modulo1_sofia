6384bcff94520dab257d6f7af70e305bfa627bd5
sosofia28 <99329422+sosofia28@users.noreply.github.com>
2022-03-11 08:34:22 -0300

    Create codigo.ino

---
+++ exercicios/ex3/codigo.ino
@@ -0,0 +1,31 @@
+// C++ code
+//
+
+void setup()
+{
+  pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(2, HIGH);
+  delay(1000); // Wait for 2 millisecond(s)
+  digitalWrite(3, HIGH);
+  delay(1000); // Wait for 2 millisecond(s)
+  digitalWrite(4, HIGH);
+  delay(1000); // Wait for 2 millisecond(s)
+  digitalWrite(5, HIGH);
+  delay(1000); // Wait for 2 millisecond(s)
+  digitalWrite(6, HIGH);
+  delay(1000); // Wait for 2 millisecond(s)
+   digitalWrite(2, LOW);
+   digitalWrite(3, LOW);
+   digitalWrite(4, LOW);
+   digitalWrite(5, LOW);
+   digitalWrite(6, LOW); 
+  delay(1000);
+}
